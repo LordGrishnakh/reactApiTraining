7ebfe493b18eb18adee5fec0ea1e3d41131bc418
IgorEXE <werewolfofmoscow@yandex.ru>
2020-05-07 14:47:42 +0300

    Project Setup

---
+++ src/SearchBar.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+
+class SearchBar extends Component {
+  state = {
+    term: "",
+  };
+
+  onFormSubmit(e) {
+    e.preventDefault();
+
+    console.log(this.state.term);
+  }
+
+  render() {
+    return (
+      <div className="ui segment">
+        <form className="ui form" onSubmit={this.onFormSubmit.bind(this)}>
+          <div>
+            <label htmlFor="search">Search</label>
+            <input
+              id="search"
+              type="text"
+              value={this.state.term}
+              onChange={(event) => this.setState({ term: event.target.value })}
+            ></input>
+          </div>
+        </form>
+        <h2>{this.state.term}</h2>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
